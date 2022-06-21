a00b2a44ab1fce237e3bf028ab458be67ff2cefa
Erin Rivas <erin@atomicbear.com>
2022-06-21 18:50:25 -0500

    testing husky

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,74 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const axios = require('axios');
+// const express = require("express");
+// const app = express();
+// const dotenv = require("dotenv").config();
+// const axios = require('axios');
 const AES = require("crypto-js/aes");
 const enc = require("crypto-js/enc-utf8");
-const { Readable } = require('stream');
+// const { Readable } = require('stream');
+
+import Express from 'express'
+import Axios from 'axios'
+import DotEnv from 'dotenv'
+import CORS from 'cors'
+
+import { Readable } from 'stream'
+import {GatewayStorageContract, UserStorageContract} from 'huskyjs'
+
+DotEnv.config()
+const app = Express()
+app.use(CORS())
+
+const contract = new GatewayStorageContract({
+    scrtContractAddr: process.env.SCRT_CONTRACT || '',
+    scrtMnemonic: process.env.SCRT_MNMONIC || '',
+    scrtRestUrl: process.env.SECRET_REST_URL || ''
+})
+
+const port = process.env.PORT || 3042
+const nodeUrl = 'jackal.squirrellogic.com'
+
+app.get('/:target/:path(.{0,})', (req, res) => {
+
+    let msg = {
+        getContentsMsg: {
+            behalf: precessDesto(req.params.target),
+            key: process.env.VK,
+            path: `${(true) ? '/www' : ''}/${req.params.path}`
+        }
+    }
 
 
-function bufferToStream(binary) {
+
+    contract.getContents(getContentsMsg).then((response: any) => {
+        let data = JSON.parse(response.file.contents);
+
+        Axios({
+            method: 'get',
+            url: `https://${nodeUrl}/download?cid=${data.cid}`,
+            responseType: 'blob'
+        })
+            .then((resp) => {
+
+                //todo this all needs to be reworked
+                const [_, content] = resp.data.split('\n')
+                const decryptContent = decrypt(content, data.key);
+
+                res.set('Cache-control', 'public, max-age=480');
+                let s = bufferToStream(Buffer.from(decryptContent));
+                s.pipe(res);
+            })
+            .catch(() => {
+                res.sendStatus(404)
+            })
+    })
+})
+app.listen(port, () => {
+    console.log(`app listening at ${port}`)
+})
+
+/** Functions */
+
+function bufferToStream (binary: Buffer) {
 
     const readableInstanceStream = new Readable({
       read() {
@@ -18,6 +79,15 @@ function bufferToStream(binary) {
 
     return readableInstanceStream;
 }
+function precessDesto (target: string) {
+    if (!target.length) {
+        throw new Error('No Target')
+    } else {
+        return (target.startsWith('secret1')) ? target : '' /** todo add RNS logic */
+    }
+}
+
+
 
 const {
     EnigmaUtils, Secp256k1Pen, SigningCosmWasmClient, pubkeyToAddress, encodeSecp256k1Pubkey
